2c8bdb5181665f57f2395863b7120df075dc1668
亗 ʜᴀʀɪ 亗 | தமிழன் | 🇮🇳 <151769010+HarixTGX@users.noreply.github.com>
2024-10-22 17:56:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,6 @@ from os import getenv
 from dotenv import load_dotenv
 
 #Necessary Variables 
-API_ID = int(getenv("API_ID", 29400566))
-API_HASH = getenv("API_HASH", "8fd30dc496aea7c14cf675f59b74ec6f")
-BOT_TOKEN = getenv("BOT_TOKEN") #Put your bot token here
-CHANNEL = getenv("CHANNEL", "ZenBotX") #Your public channel username without @ for force subscription.
-MONGO = getenv("MONGO") #Put mongo db url here
-#Optional Variables
-OWNER_ID = int(getenv("OWNER_ID", 6542673293)) #Go to @ThunderrXbot and type /id and put that value here. 
-FSUB = bool(getenv("FSUB", False)) #Set this True if you want to enable force subscription from users else set to False.
+API_ID = int(getenv("API_ID", "10261086"))
+API_HASH = getenv("API_HASH", "9195dc0591fbdb22b5711bcd1f437dab")
+BOT_TOKEN = getenv("BOT_TOKEN", "7849587328:AAG_fBi4JSm_ezIU6fcyuSwofMRS8jfg3X4") #Put your bot token here
